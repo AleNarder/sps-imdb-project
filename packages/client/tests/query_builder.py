9ede31f22f6a80ba490820e6e3866621115e9a21
Alessio Narder <alessio.narder@gmail.com>
2023-04-24 23:38:01 +0200

    feat(tsung): basic closed loop test with dynamic token substitution

--- packages/client/tests/query_builder.py
+++ packages/client/tests/query_builder.py
@@ -6,18 +6,19 @@ import random
 RATINGS_PATH = "../../data/processed/title.ratings.tsv"
 
 #%%
+print("query-builder[title.ratings.tsv]: loading data...")
 ratings_df = pd.read_csv(RATINGS_PATH, sep='\t', quotechar='"')
 movie_ids = ratings_df["tconst"]
 weights = ratings_df["prob"]
 
 #%%
+print("query-builder[title.ratings.tsv]: sampling ids...")
 sampled_movie_ids = random.choices(movie_ids, k=10_000, weights=weights)
 
 #%%
-f = open("queries.txt", "w")
-for i, sample_movie_id in enumerate(sampled_movie_ids):
-    f.write(f"{sample_movie_id}")
-    if (i != len(sampled_movie_ids) - 1):
-        f.write("\n")
+print("query-builder[title.ratings.tsv]: storing sampled ids...")
+sampled_movie_df = pd.DataFrame({ 'id': sampled_movie_ids })
+sampled_movie_df.to_csv("queries.csv", index=False, header=False)
 
-f.close()
+
+# %%
