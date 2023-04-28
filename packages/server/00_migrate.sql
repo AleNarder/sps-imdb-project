b4686637b253905676d828c71a4e0c07717032af
Giovanni De Pieri <864770@stud.unive.it>
2023-04-28 22:30:10 +0200

    Added .sql files to extends db and api functionalities

--- packages/server/00_migrate.sql
+++ packages/server/00_migrate.sql
@@ -12,8 +12,21 @@ create table imdb.title_basics (
     genres text
 );
 
+create table imdb.title_ratings (
+    tconst text primary key,
+    averageRating float,
+    numVotes integer,
+    prob float
+);
+
 COPY imdb.title_basics
 FROM '/data/title.basics.tsv'
+DELIMITER E'\t'
+QUOTE '"'
+CSV HEADER;
+
+COPY imdb.title_ratings
+FROM '/data/title.ratings.tsv'
 DELIMITER E'\t'
 QUOTE '"'
 CSV HEADER;
@@ -22,5 +35,6 @@ create role web_anon nologin;
 
 grant usage on schema imdb to web_anon;
 grant select on imdb.title_basics to web_anon;
+grant select on imdb.title_ratings to web_anon;
 
 grant web_anon to authenticator;
---
+++ packages/server/sql/02_functions.sql
@@ -0,0 +1,56 @@
+-- Create an event trigger function
+CREATE OR REPLACE FUNCTION pgrst_watch() RETURNS event_trigger
+  LANGUAGE plpgsql
+  AS $$
+BEGIN
+  NOTIFY pgrst, 'reload schema';
+END;
+$$;
+
+-- This event trigger will fire after every ddl_command_end event
+CREATE EVENT TRIGGER pgrst_watch
+  ON ddl_command_end
+  EXECUTE PROCEDURE pgrst_watch();
+
+
+
+create function imdb.get_basics_count()
+returns int
+language plpgsql
+as
+$$
+declare
+   film_count integer;
+begin
+   select count(*) 
+   into film_count
+   from imdb.title_basics;
+   
+   return film_count;
+end;
+$$;
+
+
+-- Give the size of the title_ratings table
+create  function imdb.get_ratings_count()
+returns int
+language plpgsql
+as
+$$
+declare
+   film_count integer;
+begin
+   select count(*) 
+   into film_count
+   from imdb.title_ratings;
+   
+   return film_count;
+end;
+$$;
+
+
+
+
+--grant execute on function imdb.pgrst_watch() to web_anon;
+grant execute on function "imdb".get_basics_count() to web_anon;
+grant execute on function "imdb".get_ratings_count() to web_anon;
