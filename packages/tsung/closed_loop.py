0f149122b0ae9c914c448ed2b5c1f50418031bb4
enricodarodda <enricodarodda@hotmail.it>
2023-05-28 12:22:55 +0200

    improved tsung testing configuration

--- packages/tsung/closed_loop.py
+++ packages/tsung/closed_loop.py
@@ -14,7 +14,7 @@ closed_loop = f'''<?xml version="1.0"?>
 
 <tsung loglevel="info">
     <clients>
-        <client host="localhost" use_controller_vm="true" />
+        <client host="localhost" use_controller_vm="true" maxusers="10000"/>
     </clients>
 
 
@@ -22,13 +22,24 @@ closed_loop = f'''<?xml version="1.0"?>
         <server host="{ args.host }" port="{ args.port }" type="tcp"></server>
     </servers>
 
+    <!-- The "load" section allows to define different arrival phases-->
+    <!-- The attribute "duration" is optional, and stops the load test even if
+    some sessions are still active-->
+    <!-- With "maxnumber" we limit the system to handle a certain amount of users-->
+    
+    <load duration="30" unit="minute">
+        <arrivalphase phase="1" duration="5" unit="minute" wait_all_sessions_end="true">
+            <users maxnumber="2000" arrivalrate="10" unit="second"></users>
+        </arrivalphase>
 
-    <load duration="5" unit="minute">
-        <arrivalphase phase="1" duration="10" unit="minute">
-            <users maxnumber="10" arrivalrate="2" unit="second"></users>
+        <arrivalphase phase="2" duration="6" unit="minute" wait_all_sessions_end="true">
+            <users maxnumber="5000" arrivalrate="30" unit="second"></users>
+        </arrivalphase>
+
+        <arrivalphase phase="3" duration="10" unit="minute" wait_all_sessions_end="true">
+            <users maxnumber="9500" arrivalrate="40" unit="second"></users>
         </arrivalphase>
     </load>
-
 
     <options>
         <option name="file_server" id="queries" value="./queries.csv" />
@@ -42,7 +53,7 @@ closed_loop = f'''<?xml version="1.0"?>
             </setdynvars>
 
             <for from="1" to="10000" var="i">
-                <thinktime min="2" max="10" random="true"></thinktime>
+                <thinktime min="1" max="5" random="true"></thinktime>
                 <request subst="true">
                     <http url="http://{ args.host }:{ args.port }/rpc/get_title_details?tconstvar=%%_id%%" method="GET"></http>
                 </request>
