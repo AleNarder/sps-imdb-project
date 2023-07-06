64a360ce9ddd3a2f2bb7e6c4cd96f40883e9e710
enricodarodda <enricodarodda@hotmail.it>
2023-07-06 14:14:59 +0200

    reduced core allocation from docker config file and added render functionality of tsung reports on Mac OS

--- packages/tsung/render_http_reports.sh
+++ packages/tsung/render_http_reports.sh
@@ -5,7 +5,10 @@ find ./logs/ -mindepth 2\
              -type d\
              -name '2023*'\
              -exec sh\
-             -c 'cd "{}" && pwd && if [ ! -f ./report.html ];
-                                   then
-                                        /usr/lib/x86_64-linux-gnu/tsung/bin/tsung_stats.pl;
+             -c 'cd "{}" && pwd && if [ ! -f ./report.html ]; then
+                                        if [[ $OSTYPE == 'darwin'* ]]; then
+                                             /usr/local/Cellar/tsung/1.8.0/lib/tsung/bin/tsung_stats.pl
+                                        else
+                                             /usr/lib/x86_64-linux-gnu/tsung/bin/tsung_stats.pl;
+                                        fi;
                                    fi;' \;
