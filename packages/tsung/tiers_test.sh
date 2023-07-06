78f68c2f23660096b36153afc877867debb31097
enricodarodda <enricodarodda@hotmail.it>
2023-07-06 14:16:30 +0200

    modified tiers_test.sh

--- packages/tsung/tiers_test.sh
+++ packages/tsung/tiers_test.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-for i in $(seq 1 10);
+for i in $(seq 1 5);
 do
     task run:tsung:db_test
     task run:tsung:db+be_test
     task run:tsung:db+be+rp_test
-    # task run:tsung:bottleneck_test
+done
+
+for i in $(seq 1 3);
+do
+    task run:tsung:bottleneck_test
 done
