bb77176acccd9153e000d34a32f7dc42dca673f8
Giovanni De Pieri <864770@stud.unive.it>
2023-07-07 17:18:29 +0200

    added merging and pids on plot.py

--- packages/dockerstats_script/plot.py
+++ packages/dockerstats_script/plot.py
@@ -8,18 +8,23 @@ args = parser.parse_args()
 
 
 containers = ["db1", "backend1", "nginx1", "backend2", "backend3"]
+colors=["red","blue","green","yellow","black"]
+df_merge=[]
+
 
 components = {"CPU" : ["CPU (%)", "cpu"],
 	      	  "RAM" : ["RAM (MiB)", "mem"],
 	          "RAM_percent" : ["RAM (%)", "mempercent"],
 			  "Block_I" : ["Block Input (kB)", "blockI"],
-			  "Block_O" : ["Block Output (kB)", "blockO"]}
+			  "Block_O" : ["Block Output (kB)", "blockO"],
+			  "PIDS" : ["PIDS", "pids"]}
 
-headers = ['cpu', 'mem', 'mempercent','blockI','blockO', 'date',"time"]
+headers = ['cpu', 'mem', 'mempercent','blockI','blockO','pids', 'date',"time"]
 
 try:
 	for container in containers:
 		df = pd.read_csv(args.foldername + '/'+ container +'/'+ container +'.csv', names=headers)
+		df_merge.append(df)
 		for key, value in components.items():
 			print("plotting Database " + value[0] + " usage ...")
 			plt.title(value[0])
@@ -33,3 +38,17 @@ except Exception as e:
 	print("Stats file (*.csv) needed")
 	print("Check for Database, Backend or Proxy subdirecotry existence")
 	print(e)
+
+
+
+for key, value in components.items():
+	plt.title(value[0])
+	plt.xlabel('Time (sec)') 
+	plt.ylabel(value[0])
+	index=0
+	for df in df_merge:
+		plt.plot(df["time"], df[value[1]],color=colors[index],label=containers[index])
+		index+=1
+	plt.legend(loc="center right")
+	plt.savefig(args.foldername + key + 'plot.png')
+	plt.clf()
