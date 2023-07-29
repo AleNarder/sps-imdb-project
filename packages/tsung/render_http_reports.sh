8ac84687d27de3ffb261b2684cb4a16ce44bbcd3
enricodarodda <enricodarodda@hotmail.it>
2023-07-29 16:11:48 +0200

    Fixed conditional error in report creation script

--- packages/tsung/render_http_reports.sh
+++ packages/tsung/render_http_reports.sh
@@ -6,7 +6,7 @@ find ./logs/ -mindepth 2\
              -name '2023*'\
              -exec sh\
              -c 'cd "{}" && pwd && if [ ! -f ./report.html ]; then
-                                        if [[ $OSTYPE == 'darwin'* ]]; then
+                                        if [ $OSTYPE == 'darwin'* ]; then
                                              /usr/local/Cellar/tsung/1.8.0/lib/tsung/bin/tsung_stats.pl
                                         else
                                              /usr/lib/x86_64-linux-gnu/tsung/bin/tsung_stats.pl;
