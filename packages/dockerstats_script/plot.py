0cbe1d0c604d2df744a43dabbdcd583cbe1a788c
enricodarodda <enricodarodda@hotmail.it>
2023-08-10 16:22:27 +0200

    fixed plot.py for correct unit measurement

--- packages/dockerstats_script/plot.py
+++ packages/dockerstats_script/plot.py
@@ -1,12 +1,36 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import argparse
+from colorama import init, Fore, Back, Style
 
-measurement_unit = "no measure"
+# Initializes Colorama
+init(autoreset=True)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('foldername', help = "Stats Directory")
 args = parser.parse_args()
 
+def change_unit(x, pos):
+	# global measurement_unit
+
+	# Since Data is expressed in KB
+	if x >= 1000000:
+		# measurement_unit = "GB"
+		return f"{x/1000000:.1f}"
+	elif x >= 1000:
+		# measurement_unit = "MB"
+		return f"{x/1000:.1f}"
+	else:
+		# measurement_unit = "KB"
+		return f"{x:.1f}"
+
+def set_m_unit(x):
+	if x >= 1000000:
+		return "GB"
+	elif x >= 1000:
+		return "MB"
+	else:
+		return "KB"
 
 containers = ["db1", "backend1", "nginx1", "backend2", "backend3"]
 colors=["red","blue","green","yellow","black"]
@@ -20,27 +44,7 @@ components = {"CPU" : ["CPU (%)", "cpu"],
 			  "Block_O" : ["Block Output", "blockO"],
 			  "PIDS" : ["PIDS", "pids"]}
 
-headers = ['cpu', 'mem', 'mempercent','blockI','blockO','pids', 'date',"time"]
-
-def change_unit(x, pos):
-	global measurement_unit 
-	if x >= 1000000:
-		measurement_unit = "GB"
-		return f"{x/1000000:.1f} GB"
-	elif x >= 1000:
-		measurement_unit = "MB"
-		return f"{x/1000:.1f} MB"
-	else:
-		measurement_unit = "KB"
-		return f"{x:.1f} KB"
-def change_m_unit(x):
-	global measurement_unit 
-	if x >= 1000000:
-		measurement_unit = "GB"
-	elif x >= 1000:
-		measurement_unit = "MB"
-	else:
-		measurement_unit = "KB"
+headers = ['cpu','mem','mempercent','blockI','blockO','pids','date','time']
 
 try:
 	for container in containers:
@@ -51,38 +55,42 @@ try:
 			plt.title(value[0])
 			plt.xlabel('Time (sec)') 
 			plt.ylabel(value[0])
-			plt.plot(df["time"], df[value[1]])
+			plt.plot(df['time'], df[value[1]])
 
 			if key == "Block_I" or key == "Block_O":
 				ax = plt.gca()
 				ax.yaxis.set_major_formatter(change_unit)
-				change_m_unit(df[value[1]].max())
+				measurement_unit = set_m_unit(df[value[1]].max())
+				# and update the plot title with the correct measurement unit
 				plt.title(f"{value[0]} ({measurement_unit})")
 				
 			plt.savefig(f"{args.foldername}/{container}/{key}plot.svg")
 			plt.clf()
 
 except Exception as e:
-	print("Stats file (*.csv) needed")
-	print("Check for Database, Backend or Proxy subdirecotry existence")
-	print(e)
+	print(Fore.YELLOW + "Stats file (*.csv) needed \nCheck for Database, Backend or Proxy subdirecotry existence")
+	print(Fore.RED + str(e))
 	pass
 
 
 # Create comparison plots
 for key, value in components.items():
+	print(f"plotting Comparison Plots: {value[0]}")
 	plt.title(value[0])
 	plt.xlabel('Time (sec)') 
 	plt.ylabel(value[0])
-	index=0
+	index = 0
+	max = 0
 	for df in df_merge:
-		plt.plot(df["time"], df[value[1]],color=colors[index],label=containers[index])
-		index+=1
+		plt.plot(df['time'], df[value[1]], color=colors[index], label=containers[index])
+		max = df[value[1]].max() if max < df[value[1]].max() else max
+		index += 1
 	plt.legend(loc="center right")
 
 	if key == "Block_I" or key == "Block_O":
 		ax = plt.gca()
 		ax.yaxis.set_major_formatter(change_unit)
+		measurement_unit = set_m_unit(max)
 		plt.title(f"{value[0]} ({measurement_unit})")
 
 	plt.savefig(f"{args.foldername}/comparison_plots/{key}plot.svg")
